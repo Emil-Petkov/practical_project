54773b89a8c3e244b7c76f2d9408a25df0af8f5b
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2022-10-01 18:35:51 +0300

    tip_calculator

---
+++ tip_calculator/tip_calculator.py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator!")
+print()
+bill = float(input("What was the total bill? $: "))
+tip = int(input("How much tip would you like to give? 0, 5, 10, 12, 15: "))
+people = int(input("How many people to split the bill?: "))
+
+tip_as_percent = (bill * tip) / 100
+total_per_person = (bill + tip_as_percent) / people
+print()
+print(f"${total_per_person:.2f}")
