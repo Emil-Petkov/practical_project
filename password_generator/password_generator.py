ab64c63956cdae65ae2949396b5b6bc09b01e27a
hellhound <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-05 01:03:44 +0300

    for fun : )

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -1,25 +1,37 @@
-import random
 import string
-
-print("Welcome to my Password Generator : )\n")
-
-n_n = int(input("How many numbers would you like in your password? "))
-n_l = int(input("How many letters would you like in your password? "))
-n_c = int(input("How many symbols would you like in your password? "))
-
-numbers = string.digits
-letters = string.ascii_letters
-symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "<", ">", "{", "}", "[", "]", "|"]
+import random
+from ascii_art import asart
+import time
 
 password = []
 
-for n in range(1, n_n + 1):
-    password.append(random.choice(numbers))
-for l in range(1, n_l + 1):
-    password.append(random.choice(letters))
-for c in range(1, n_c + 1):
-    password.append(random.choice(symbols))
 
-print()
-random.shuffle(password)
-print(f'This is your password: {"".join(password)}')
+def password_generator(n_letters: int, n_numbers: int, n_symbols: int):
+    l = string.ascii_letters
+    n = string.digits
+    s = ["!", "@", "#", "$", "%", "&", "*"]
+
+    for letter in range(0, n_letters):
+        password.append(random.choice(l))
+
+    for num in range(0, n_numbers):
+        password.append(random.choice(n))
+
+    for sym in range(0, n_symbols):
+        password.append(random.choice(s))
+
+    random.shuffle(password)
+
+    print(43 * "_")
+    return f"This is your random password: {''.join(password)}"
+
+
+print(asart)
+n_letters = int(input("How many letters you want in your password? "))
+n_numbers = int(input("How many numbers you want in your password? "))
+n_symbols = int(input("How many symbols you want in your password? "))
+
+result = password_generator(n_letters, n_numbers, n_symbols)
+print(result)
+time.sleep(92)
+
