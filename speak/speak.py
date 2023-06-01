2c12dbf42f84e4bb892d1a8753eb708d7522ed47
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-01 16:14:36 +0300

    :)

--- speak/speak.py
+++ speak/speak.py
@@ -11,14 +11,20 @@ engine = pyttsx3.init()
 
 print("""* For exit() on the program write End command.
 ______________________________________________
+
+Write anything and your computer will read the message.
+** Only English language is supported. **
 """)
 
-text = input(">>> ")
+text = input("🤖: ")
 
 while text != "End" and text != "END" and text != "end":
     speak_python(text)
-    text = input(">>> ")
+    text = input("🤖: ")
 
-print("\nHave a nice day my friend : )")
+print("""\n🤖: Have a nice day my friend : )
+_________________________________""")
 speak_python("Have a nice day my friend : )")
+
+print("\nТhe program will close automatically after 5 seconds.")
 time.sleep(5)
